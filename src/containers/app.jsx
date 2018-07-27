e58cde4f9691fb3effffba0ecf1838cf2e11e29e
chen8000 <13126618000@163.com>
2018-07-27 19:46:24 +0800

    基本结构

---
+++ src/containers/app.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+class App extends Component {
+
+    render(){
+        return (
+            <div>
+                hello Orange
+            </div>
+        )
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.scss'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+
+
+import App from './containers/app'
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+
+
