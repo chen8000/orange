f14d7428fe4e16627aa173ea8cca4002d4ea345f
chen8000 <13126618000@163.com>
2018-07-30 12:56:19 +0800

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@
 
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+
+// 使用redux调试工具
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import reducers from './reducers'
