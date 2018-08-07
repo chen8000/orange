cb66bca85c3d68782aa22ed8b1f770eace2de7a0
chen8000 <13126618000@163.com>
2018-08-07 14:54:03 +0800

    修改占位组件为无状态组件

--- src/components/commonComponents/placeholder/index.jsx
+++ src/components/commonComponents/placeholder/index.jsx
@@ -1,19 +1,15 @@
 
+///////////   无状态组件    //////
 
 // 占位组件
 // 同比例撑起父元素
 // 以便父元素内的其他元素进行定位而不乱版
 
-import React, { Component } from 'react'
+import React from 'react'
 
 import style from './index.scss'
 
-class Placeholder extends Component{
-
-    render(){
-        return <img className={ style.placeholder } src={ this.props.url } alt=""/>
-    }
-}
+const Placeholder = ( props ) => <img className={ style.placeholder } src={ props.url } alt=""/>
 
 export default Placeholder
 
