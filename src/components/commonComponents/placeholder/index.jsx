2ca2b086684f0d59457e6601fd5d1bfb840f879b
chen8000 <13126618000@163.com>
2018-08-07 15:06:58 +0800

    占位组件

--- src/components/commonComponents/placeholder/index.jsx
+++ src/components/commonComponents/placeholder/index.jsx
@@ -1,15 +1,19 @@
 
-///////////   无状态组件    //////
 
 // 占位组件
 // 同比例撑起父元素
 // 以便父元素内的其他元素进行定位而不乱版
 
-import React from 'react'
+import React, { Component } from 'react'
 
 import style from './index.scss'
 
-const Placeholder = ( props ) => <img className={ style.placeholder } src={ props.url } alt=""/>
+class Placeholder extends Component{
+
+    render(){
+        return <img className={ style.placeholder } src={ this.props.url } alt=""/>
+    }
+}
 
 export default Placeholder
 
