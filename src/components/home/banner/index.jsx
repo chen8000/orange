88f9a2f3bc37a9472c9f4b0ab3f51849e39eeec4
chen8000 <13126618000@163.com>
2018-08-09 01:16:22 +0800

    banner按钮

--- src/components/home/banner/index.jsx
+++ src/components/home/banner/index.jsx
@@ -8,8 +8,16 @@ class Banner extends Component {
             <div className={ style.banner }>
                 <img className={ style.bannerBg } src="/img/home/banner/banner.jpg" alt=""/>
                 <div className={ style.linkCase }>
-                    <a href=""><i className="iconfont icon-github3"></i>GitHub</a><br/>
-                    <a href=""><i className="iconfont icon-bokeyuan"></i>博客园</a>
+                    <a href="https://github.com/chen8000" target="_blank" rel="noopener noreferrer">
+                        <i className="iconfont icon-github3"></i>
+                        GitHub
+                        <span className="iconfont icon-jiantou1-copy"></span>
+                    </a>
+                    <a href="https://www.cnblogs.com/chefweb/" target="_blank" rel="noopener noreferrer">
+                        <i className="iconfont icon-bokeyuan"></i>
+                        博客园
+                        <span className="iconfont icon-jiantou1-copy"></span>
+                    </a>
                 </div>
             </div>
         )
