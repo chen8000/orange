e476bda05fc1442da407c5de4e30590b4b6eb4f1
zhanghui.chen <13126618000@163.com>
2018-08-22 18:24:19 +0800

    随机显示背景图片

--- src/components/detail/index.jsx
+++ src/components/detail/index.jsx
@@ -1,14 +1,26 @@
 
 import React, { Component } from 'react'
-// import style from './index.scss'
+import style from './index.scss'
 
 class Detail extends Component {
+
+    constructor(props){
+        super(props)
+        
+        this.state = {
+
+            // 随机显示图片
+            bg: Math.floor ( Math.random ( ) * 11 + 1)
+        }
+    }
 
     render(){
         
         return (
             <div>
-                detail
+                <div  className={ style.bg }>
+                    <img src= { `/img/detail/bg/item${this.state.bg}.jpg` } alt=""/>
+                </div>
             </div>
         )
     }
