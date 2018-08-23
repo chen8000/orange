d268b16abf00e6a33dbb913b2efb27a0e69d8277
zhanghui.chen <13126618000@163.com>
2018-08-23 13:26:07 +0800

    文章title

--- src/components/detail/index.jsx
+++ src/components/detail/index.jsx
@@ -4,23 +4,31 @@ import style from './index.scss'
 
 class Detail extends Component {
 
-    constructor(props){
-        super(props)
-        
-        this.state = {
-
-            // 随机显示图片
-            bg: Math.floor ( Math.random ( ) * 11 + 1)
-        }
-    }
-
     render(){
-        
         return (
             <div>
-                <div  className={ style.bg }>
-                    <img src= { `/img/detail/bg/item${this.state.bg}.jpg` } alt=""/>
+                <div  className={ style.headerbar }>
+                    {/* 背景图 */}
+                    <img className={ style.bgImg } src= { `/img/detail/bg/item1.jpg` } alt=""/>
+
+                    {/* info */}
+                    <div className={ style.info }>
+                        <h2>OPPO Find X兰博基尼版开箱图赏！超跑奢华质感，大写的牛！</h2>
+                        <div className={ style.head }>
+                            <div className={ style.headImg }>
+                                <img src="/img/circle/article/todos/headImg.jpg" alt=""/>
+                            </div>
+                            <div className={ style.namebox }>
+                                <h3>zhenghui.chen<i className="iconfont icon-dengji1"></i></h3>
+                                <p>前端开发工程师</p>
+                            </div>
+                            <div className={ style.timesbox }>
+                                <span>2018年 8月23日 18:35</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <div style={{marginTop:'500px'}}></div>
             </div>
         )
     }
