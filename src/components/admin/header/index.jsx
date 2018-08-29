c833597d0ae5d894728912837bdc5e5d5a1d2f0a
zhanghui.chen <13126618000@163.com>
2018-08-29 19:04:44 +0800

    管理页面开发

--- src/components/admin/header/index.jsx
+++ src/components/admin/header/index.jsx
@@ -10,7 +10,29 @@ class Header extends Component {
                     <img src="/img/admin/header/logo.png" alt=""/>
                 </div>
                 <div className={ style.right }>
+                    {/* 设置 */}
+                    <div className={ style.item }>
+                        <div className={ style.setting }>
+                            <i className="iconfont icon-shezhi1"></i>
+                        </div>
+                    </div>
 
+                    {/* 退出登陆 */}
+                    <div className={ style.item }>
+                        <div className={ style.exit }>
+                            <i className="iconfont icon-tuichu2"></i>
+                            <span>安全退出</span>    
+                        </div>
+                    </div>
+
+                    {/* 头像 */}
+                    <div className={ style.item }>
+                        <div className={ style.ava }>
+                            <img src="/img/circle/article/todos/headImg.jpg" alt=""/>
+                        </div>
+                    </div>
+                    
+                    
                 </div>
             </div>
         )
